6e48c627cd715b8e275083f8b08f8a386c254853
cooldadpresident <bumabuch@gmail.com>
2024-04-19 21:02:37 +0200

    workshpop

--- server/index.mjs
+++ server/index.mjs
@@ -2,18 +2,23 @@ import WebSocket, { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({
   port: 8400,
-});
+}); 
 console.info("connected");
 const users = [];
-
+let id = 0; 
 wss.on("connection", function connection(user_connection) {
-  users.push(user_connection);
+  users.push({user_connection, id});
   console.info("New connection");
+  const userId = id;
+  id++;
+
+
+
 
   user_connection.on("message", function message(data) {
     console.info("Broadcasting message to ${users.length} users");
     users.forEach((user) => {
-      user.send(`${data}`);
+      user.user_connection.send(`${data}, ${userId}`);
     });
   });
 });
