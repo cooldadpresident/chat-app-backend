7b53f54eca24353cdf1a9ea8a9f167fe953e512e
cooldadpresident <bumabuch@gmail.com>
2024-04-29 09:32:31 +0200

    chat app, user name

--- server/index.mjs
+++ server/index.mjs
@@ -12,9 +12,6 @@ wss.on("connection", function connection(user_connection) {
   const userId = id;
   id++;
 
-
-
-
   user_connection.on("message", function message(data) {
     console.info("Broadcasting message to ${users.length} users");
     users.forEach((user) => {
